b23b6f784dd111cf3150c8b9a04ccc13926fd031
Praphul <praphul001@gmail.com>
2026-02-22 11:57:43 +0530

    Added Softmax function

--- nn.py
+++ nn.py
@@ -3,21 +3,36 @@ from nnfs.datasets import spiral_data
 class DenseLayer:
     
     def __init__(self,n_inputs,n_neuros) :
-        
+        # Intialize the weights and the biases
         self.weights = np.random.randn(n_inputs,n_neuros)*0.01
         self.biases = np.zeros((1,n_neuros))
         
     def forward(self,inputs):
+        # Forward pass
         self.output = np.dot(inputs,self.weights) + self.biases
 
 
-
+class ReLU:
+    
+   def forward(self,inputs):
+       # Forward pass with activation function
+       self.output = np.maximum(0,inputs)
+       
+class SoftMax:
+    
+    def forward(self,inputs):
+        inputs = inputs - np.max(inputs,axis=1,keepdims=True)
+        self.output = np.exp(inputs)/np.sum(np.exp(inputs),axis=1,keepdims=True)
+        
 # Create dataset
 
 X,y = spiral_data(samples=100,classes=3)
 
-dense1 = DenseLayer(2,4)
+dense1 = DenseLayer(2,3)
 
 dense1.forward(X)
 
-print(dense1.output)
+softmax_activation = SoftMax()
+
+softmax_activation.forward(dense1.output)
+print(softmax_activation.output)
